5aab1b25666af6734b471a63b770cd5132dc2971
39Ottoman <to_the_top12345@yahoo.co.jp>
2015-04-08 00:15:20 +0900

    modify search page.

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -18,11 +18,15 @@ function findTodo() {
     var $results = $('#results');
     var $message = $('#resultMessage');
     $results.fadeOut(function() {
-        $results.children().remove();
+        // メッセージを初期化
+        $message.text('');
         
         // /todosにGETでアクセス
         $.get('/todos/?name=' + $query.val(), function(todos) {
             console.log(todos);
+            // 一覧を初期化
+            $results.children().remove();
+            
             // Todoがなければメッセージを表示
             if(todos.length === 0) {
                 $message.text('対象のToDoは見つかりません');
@@ -30,20 +34,19 @@ function findTodo() {
             } else {
                 $message.text(todos.length + '件見つかりました');
                 $message.css('color', 'black');
+                // 取得したToDoがあれば表示
+                $.each(todos, function(index, todo) {
+                    // ToDoの所属しているリストを取得
+                    var todoList = findTodoList(todo.listId, todoLists);
+                    var todoButton = '<div class="panel">'
+                        + '<h3>' + todo.name + '</h3>'
+                        + '期限：　' + new Date(todo.limitDate).toLocaleDateString('ja-JP') + '</br>'
+                        + '作成日：' +  new Date(todo.createdDate).toLocaleDateString('ja-JP') + '</br>'
+                        + 'リスト：<a href="/todopage/' + todoList._id + '">' + todoList.name + '</a></br>'
+                        + '</div>';
+                    $results.prepend(todoButton);
+                });
             }
-            // 取得したToDoがあれば表示
-            $.each(todos, function(index, todo) {
-                // ToDoの所属しているリストを取得
-                var todoList = findTodoList(todo.listId, todoLists);
-                var todoButton = '<div class="panel">'
-                    + '<h3>' + todo.name + '</h3>'
-                    + '期限：　' + new Date(todo.limitDate).toLocaleDateString('ja-JP') + '</br>'
-                    + '作成日：' +  new Date(todo.createdDate).toLocaleDateString('ja-JP') + '</br>'
-                    + '<input type="button" value="' + (todo.isCheck? '完了': '未完了') + '"/></br>'
-                    + 'リスト：<a href="/todopage/' + todoList._id + '">' + todoList.name + '</a></br>'
-                    + '</div>';
-                $results.prepend(todoButton);
-            });
         });
     });
     $results.fadeIn();
